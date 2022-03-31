a2c920d2f9f936d28a97c820b68766febf359c37
pddec <73551981+pddec@users.noreply.github.com>
2022-03-31 06:37:57 -0300

    Commit innit

---
+++ projects/src/com/ui/clock/pluggin/views/ClockView.java
@@ -0,0 +1,48 @@
+package com.ui.clock.pluggin.views;
+
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.PaintEvent;
+import org.eclipse.swt.widgets.Canvas;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.ui.part.ViewPart;
+
+/**
+ * This sample class demonstrates how to plug-in a new workbench view. The view
+ * shows data obtained from the model. The sample creates a dummy model on the
+ * fly, but a real implementation would connect to the model available either in
+ * this or another plug-in (e.g. the workspace). The view is connected to the
+ * model using a content provider.
+ * <p>
+ * The view uses a label provider to define how model objects should be
+ * presented in the view. Each view can present the same model objects using
+ * different labels and icons, if needed. Alternatively, a single label provider
+ * can be shared between views in order to ensure that objects of the same type
+ * are presented in the same way everywhere.
+ * <p>
+ */
+
+public class ClockView extends ViewPart {
+
+	/**
+	 * The ID of the view as specified by the extension.
+	 */
+	public static final String ID = "com.ui.clock.pluggin.views.ClockView";
+
+	@Override
+	public void createPartControl(final Composite parent) {
+
+		final Canvas clock = new Canvas(parent, SWT.NONE);
+		clock.addPaintListener(this::drawClock);
+
+	}
+
+
+	private void drawClock(final PaintEvent event) {
+		event.gc.drawArc(event.x, event.y, event.width - 1, event.height - 1, 0, 360);
+	}
+
+	@Override
+	public void setFocus() {
+
+	}
+}
