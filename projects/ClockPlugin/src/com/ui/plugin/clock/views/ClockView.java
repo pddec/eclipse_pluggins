cf73ed40f330adc09011234ef1a11cddea4b1de6
pddec <73551981+pddec@users.noreply.github.com>
2022-03-31 08:18:14 -0300

    Creating clock widget

--- projects/ClockPlugin/src/com/ui/plugin/clock/views/ClockView.java
+++ projects/ClockPlugin/src/com/ui/plugin/clock/views/ClockView.java
@@ -1,13 +1,10 @@
 package com.ui.plugin.clock.views;
 
-import java.time.LocalTime;
-
 import org.eclipse.swt.SWT;
-import org.eclipse.swt.events.PaintEvent;
-import org.eclipse.swt.graphics.Color;
-import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.ui.part.ViewPart;
+
+import com.ui.plugin.clock.widget.ClockWidget;
 
 /**
  * This sample class demonstrates how to plug-in a new workbench view. The view
@@ -33,47 +30,14 @@ public class ClockView extends ViewPart {
 
 	@Override
 	public void createPartControl(final Composite parent) {
-		final Canvas clock = new Canvas(parent, SWT.NONE);
-		clock.addPaintListener(this::drawClock);
-		clock.addPaintListener(this::paintControl);
 		
-		ClockView.moveSecondHand(clock).start();
+		final ClockWidget clockWidget = ClockWidget.builder()
+		.parent(parent)
+		.style(SWT.NONE)
+		.build();
 		
-	}
-
-	private void drawClock(final PaintEvent event) {
-		event.gc.drawArc(event.x, event.y, event.width - 1, event.height - 1, 0, 360);
-	}
-
-	public void paintControl(final PaintEvent event) {
-		event.gc.drawArc(event.x, event.y, event.width - 1, event.height - 1, 0, 360);
-		final int seconds = LocalTime.now().getSecond();
-		final int arc = (15 - seconds) * 6 % 360;
-		final Color blue = event.display.getSystemColor(SWT.COLOR_BLUE);
-		event.gc.setBackground(blue);
-		event.gc.fillArc(event.x, event.y, event.width - 1, event.height - 1, arc - 1, 2);
-	}
-	
-	private static Thread moveSecondHand(final Canvas clock) {
-		
-		final Runnable runClock = () ->{
-			while (!clock.isDisposed()) {
-				try {
-					//clock.redraw();
-					clock.getDisplay().asyncExec(() -> clock.redraw());
-					Thread.sleep(1000);
-				}catch (Exception e) {
-					throw new RuntimeException(e);
-				}
-			}
-		};
-		
-		final Thread runner = new Thread(runClock,"Tick Tack");
-		runner.setUncaughtExceptionHandler((thread, exception) -> {
-			exception.printStackTrace();
-		});
-		
-		return runner;
+		clockWidget.initPaintListener();
+		clockWidget.moveSecondHand().start();
 		
 	}
 
